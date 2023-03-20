6c470b962523be0e0d2ab8ffab447a748afd97a1
Nate Schultz <nate.schultz@outlook.com>
2023-03-20 19:42:24 -0400

    pink square

--- src/main.py
+++ src/main.py
@@ -7,5 +7,13 @@ print(uuid)
 
 width, height = 100, 100
 image = np.zeros((width, height, 3), dtype=np.uint8)
-image[75, 75, :] = (0xFF, 0xFF, 0xFF)
+
+def update_pixel(x, y, r, g, b, image):
+    image[x, y, :] = (0xFF * r, 0xF * g, 0xFF * b)
+    return image
+
+for x in range(25, 75):
+    for y in range(25, 75):
+        image = update_pixel(x, y, y/x, x/y, (x+y)/2, image)
+
 imageio.imwrite('output.png', image)
