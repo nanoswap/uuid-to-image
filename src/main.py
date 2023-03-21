c795a8040239e2086d8e481ab04fa67f3f3b1a7f
Nate Schultz <nate.schultz@outlook.com>
2023-03-20 20:33:34 -0400

    put it all in a function

--- src/main.py
+++ src/main.py
@@ -3,12 +3,6 @@ import numpy as np
 import imageio
 import math
 
-uuid = uuid.uuid4()
-print(uuid)
-
-width, height = 100, 100
-image = np.zeros((width, height, 3), dtype=np.uint8)
-
 def update_pixel(x, y, r, g, b, image):
     image[x, y, :] = ((0xFF * r, 0xF * g, 0xFF * b) + image[x, y, :])/2
     return image
@@ -16,29 +10,42 @@ def update_pixel(x, y, r, g, b, image):
 def distance(x1, y1, x2, y2):
     return math.sqrt(math.pow(x2 - x1, 2) + math.pow(y2 - y1, 2))
 
-uuid_str = str(uuid).replace('-', '')
+def generate_image(width, height, uuid):
+    image = np.zeros((width, height, 3), dtype=np.uint8)
 
-last = 0
-for a, b, c in zip(uuid_str, uuid_str[1:], uuid_str[2:]):
+    uuid_str = str(uuid).replace('-', '')
 
-    red = int(a, 16) / 16
-    green = int(b, 16) / 16
-    blue = int(c, 16) / 16
+    last = 0
+    for a, b, c in zip(uuid_str, uuid_str[1:], uuid_str[2:]):
 
-    x_loc = int(red+green * width)
-    y_loc = int(green+blue * height)
-    size = int((red + green + blue) * 16)
-    last += x_loc / (last + 1)
+        red = int(a, 16) / 16
+        green = int(b, 16) / 16
+        blue = int(c, 16) / 16
 
-    for x in range(x_loc - size, x_loc + size):
-        for y in range(y_loc - size, y_loc + size):
-            if x < width and y < height and x > 0 and y > 0:
-                if distance(x_loc, y_loc, x, y) < size or a > b:
-                    if int(a, 16) / (int(b, 16) + 1) > int(c, 16):
-                        image = update_pixel(x, y, red - green*width, green, blue, image)
-                    elif int(c, 16) / (int(a, 16) + 1) < int(b, 16):
-                        image = update_pixel(x, y, green, red - blue*height, blue, image)
-                    else:
-                        image = update_pixel(x, y, red - blue*height, blue - red*height, green * red*width, image)
+        x_loc = int(red+green * width)
+        y_loc = int(green+blue * height)
+        size = int((red + green + blue) * 16)
+        last += x_loc / (last + 1)
 
+        for x in range(x_loc - size, x_loc + size):
+            for y in range(y_loc - size, y_loc + size):
+                if x < width and y < height and x > 0 and y > 0:
+                    if distance(x_loc, y_loc, x, y) < size or a > b:
+                        if int(a, 16) / (int(b, 16) + 1) > int(c, 16):
+                            image = update_pixel(x, y, red - green*width, green, blue, image)
+                        elif int(c, 16) / (int(a, 16) + 1) < int(b, 16):
+                            image = update_pixel(x, y, green, red - blue*height, blue, image)
+                        else:
+                            image = update_pixel(x, y, red - blue*height, blue - red*height, green * red*width, image)
+
+    return image
+
+cur_uuid = uuid.uuid4()
+print(cur_uuid)
+
+# for testing with a static uuid to make sure it's idempotent
+# cur_uuid = uuid.UUID("4d28f930-9e4e-43ef-9fad-bc09da456bb4")
+
+width, height = 100, 100
+image = generate_image(width, height, cur_uuid)
 imageio.imwrite('output.png', image)
